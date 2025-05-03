c37cfc9dcf355e8a39317edcbe7496f4217382c4
Monjurul Alam <monjurulalam190629@gmail.com>
2025-05-03 22:26:44 +0600

    Backend is now set up

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+// Import core modules
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+// load environment variables from .env
+dotenv.config();
+
+// define port
+const PORT = process.env.PORT | 5000;
+
+// Initialize Express App
+const app = express();
+
+
+// Enable cors for all routes
+app.use(cors());
+
+// Enable incoming json requests
+app.use(express.json());
+
+
+// default for checking
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Server is running now');
+})
+
